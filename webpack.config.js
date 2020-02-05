ce34986a93c28f014c4bfc6addf84d73c665dd39
Luciad Developer Platform <60262140+luciaddeveloperplatform@users.noreply.github.com>
2020-02-05 02:47:11 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@
  (1) In your babel-loader make sure you exclude anything outside /src folder or your compilation time will grow (include /src/ )
  (2) Configure your license in a separate entry in the entry array and makes sure it placed before that your main script.
  (3) Disable your map creation in production mode or you will end with a huge file )
+ (4) Configure an Uglification plugin as terser, make sure compress.unused is set to false
  ********************************************************************************************************************************/
 
 const path = require('path');
