bccc6d3f892c641e4e3726fd22da4864ed0daa99
Luciad Developer Platform <60262140+luciaddeveloperplatform@users.noreply.github.com>
2020-02-12 00:59:16 +0100

    Update index.js

--- src/license/index.js
+++ src/license/index.js
@@ -1,6 +1,6 @@
 import { setLicenseText } from "@luciad/ria/util/License";
 
-const licenseText = require("!!url-loader!./luciadria_development.txt").default;
+const licenseText = require("!!url-loader!../../licenses/luciadria_development.txt").default;
 
 function decodeBase64StringToPlainText(licenseBase64) {
   // Base64 String has the format: data:text/plain;base64,code
